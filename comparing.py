ce2a46345b171554fecea5b589755aee23a1f342
kubusbubu <kubakarczewski25@gmail.com>
2023-05-15 00:16:07 +0200

    Fixed sth idk

--- comparing.py
+++ comparing.py
@@ -3,6 +3,8 @@ from tools import date_format_standardization
 import logging
 import os
 from tools import logger_function
+import json
+import csv
 
 logger_function()
 log = logging.getLogger(__name__)
@@ -33,7 +35,7 @@ def date_format_check(date, format) -> None:
         print("Invalid date format")
 
 
-# outpuy_dir = [pliki_csv]
+# sprawdz nazwy kolumn
 def check_column_names(dataframe, json_file):
     # csv -> pandas
     df_columns = list(dataframe.columns)
@@ -44,6 +46,7 @@ def check_column_names(dataframe, json_file):
             log.warning("Wrong column name in " + " dataframe, column name : " + json_file[column]["name"])
 
 
+# sprawdz typy kolumn - > to nie bedzie dzialac
 def check_column_types(dataframe, json_file):
     # csv -> pandas
     df_columns = list(dataframe.columns)
@@ -51,4 +54,38 @@ def check_column_types(dataframe, json_file):
     for column in range(len(json_file)):
         if json_file[column]["type"] != df_columns[column]:
             # blad powinien byc rozwiniety jescze o nazwe pliku i nazwe kolumny
-            log.warning("Wrong column name in " + " dataframe, column name : " + json_file[column]["name"])
+            log.warning("Wrong column type in " + " dataframe, column name : " + json_file[column]["name"])
+
+
+# sprawdz wartosci w kolumnach
+def check_dataframe_types(dataframe, json_file):
+    # Read the JSON file
+
+    # with open(json_file, 'r') as f:
+    #     column_types = json.load(f)
+    df_columns = list(dataframe.columns)
+    del df_columns[0]
+    
+    for ind, column_name in enumerate(df_columns):
+        # Get the expected type for the current column
+        
+        # expected_type = column_types.get(column_name)
+        expected_type = json_file[ind]["type"]
+
+        if expected_type is None:
+            raise ValueError(f"No type information found for column '{column_name}' in the JSON file.")
+        
+        # Check the type of each value in the column
+        # Add log information
+        values = dataframe[column_name]
+        if expected_type == 'STRING' and not values.apply(lambda x: isinstance(x, str)).all():
+            return False
+        elif expected_type == 'DATE' and not values.apply(lambda x: isinstance(x, str)).all():
+            return False  # Adjust this condition based on your date format
+        elif expected_type == 'DOUBLE' and not values.apply(lambda x: isinstance(x, float)).all():
+            return False
+        elif expected_type == 'INTEGER' and not values.apply(lambda x: isinstance(x, int)).all():
+            return False
+    
+    return True  # All values in all columns are of the correct types
+
